37ca9acd9ec53e86707fbdba08c1d25f93578d48
Sneha <Sneha@DESKTOP-04U12V3>
2024-03-04 16:14:19 +0530

    hi

---
+++ Demo/src/com/march4/Info.java
@@ -0,0 +1,13 @@
+package com.march4;
+
+public class Info {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name="sneha";
+		
+		System.out.println(name);
+	}
+
+}
