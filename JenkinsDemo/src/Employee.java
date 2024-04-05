2760fbc87d90934e14f2d3682b901d4256408c83
Sneha <Sneha@DESKTOP-04U12V3>
2024-04-05 17:58:46 +0530

    sneha13

---
+++ JenkinsDemo/src/Employee.java
@@ -0,0 +1,11 @@
+
+public class Employee {
+
+	public static void main(String[] args) 
+	{
+		String fname="sneha";
+		String lname="Dhulap";
+		System.out.println(fname+" "+lname);
+	}
+
+}
---
+++ JenkinsDemo/src/Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		String name="sneha";
+		int Roll_no=120;
+		
+		System.out.println(name+" "+Roll_no);
+
+	}
+
+}
