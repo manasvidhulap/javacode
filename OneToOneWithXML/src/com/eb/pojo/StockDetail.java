d9f49097b9f94bb88606ddc2e58ac02548463d79
Sneha <Sneha@DESKTOP-04U12V3>
2024-04-09 19:47:34 +0530

    sneha14

---
+++ OneToOneWithXML/src/com/eb/pojo/StockDetail.java
@@ -0,0 +1,49 @@
+package com.eb.pojo;
+
+import java.util.Date;
+
+public class StockDetail {
+	private Integer stockId;
+	
+	private String compName;
+	private String compDesc;
+	private String remark;
+	private Date listedDate;
+	private Stock stock;
+	public Integer getStockId() {
+		return stockId;
+	}
+	public void setStockId(Integer stockId) {
+		this.stockId = stockId;
+	}
+	public Stock getStock() {
+		return stock;
+	}
+	public void setStock(Stock stock) {
+		this.stock = stock;
+	}
+	public String getCompName() {
+		return compName;
+	}
+	public void setCompName(String compName) {
+		this.compName = compName;
+	}
+	public String getCompDesc() {
+		return compDesc;
+	}
+	public void setCompDesc(String compDesc) {
+		this.compDesc = compDesc;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public Date getListedDate() {
+		return listedDate;
+	}
+	public void setListedDate(Date listedDate) {
+		this.listedDate = listedDate;
+	}
+}
